f2906fd52c4c29b4dca35fbfdd730da383e6be0f
cice <a>
2017-02-02 18:33:04 +0100

    Añadido Contraints por código: creadas 2 vistas, padre e hija, e igualada las alturas, anchos, centerX y CenterY la hija y la padre

--- Constraints/ViewController.m
+++ Constraints/ViewController.m
@@ -16,7 +16,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Creadas 2 vistas Padre e hija y añadida la hija dentro de padre
+    UIView *vistaHija = [[UIView alloc] init];
+    UIView *vistaPadre = self.view;
+    vistaHija.translatesAutoresizingMaskIntoConstraints = false;
+    vistaHija.backgroundColor = [UIColor redColor];
+    [vistaPadre addSubview:vistaHija];
+    
+    // Añadidos 4 constraints Ancho y alttura hija igual padre y CenterX y CenterY
+    [vistaPadre addConstraint:[NSLayoutConstraint constraintWithItem:vistaPadre attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:vistaHija attribute:NSLayoutAttributeWidth multiplier:1 constant:0]];
+    
+    [vistaPadre addConstraint:[NSLayoutConstraint constraintWithItem:vistaPadre attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:vistaHija attribute:NSLayoutAttributeHeight multiplier:1 constant:0]];
+    
+    [vistaPadre addConstraint:[NSLayoutConstraint constraintWithItem:vistaPadre attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:vistaHija attribute:NSLayoutAttributeCenterX multiplier:1 constant:0]];
+    
+    [vistaPadre addConstraint:[NSLayoutConstraint constraintWithItem:vistaPadre attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:vistaHija attribute:NSLayoutAttributeCenterY multiplier:1 constant:0]];
+    
 }
 
 
